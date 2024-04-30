1151ca517a1fb8c4bead13e1808729ced90fa452
hsc996 <han.scaife@gmail.com>
2024-04-30 12:36:25 +1000

    main menu function added

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ import os
 # External packages
 from colored import Fore, Back, Style # type:ignore
 
+# Import custom functions
+
+
 def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
@@ -88,6 +91,7 @@ HANGMAN = [
 '''
 ]
 
+
 class Hangman:
     def __init__(self, word):
         self.word = word.upper()
@@ -96,45 +100,47 @@ class Hangman:
         self.current_progress = ['_' if letter.isalpha() else letter for letter in self.word]
         self.hangman_stage = 0
 
+
     def guess(self, letter):
         letter = letter.upper()
         if letter in self.guessed_letters:
-            print(f"\n{Fore.red}You've already guessed that letter!{Style.reset}")
+            print(f"\n{Fore.RED}You've already guessed that letter!{Style.RESET}")
             return
 
         self.guessed_letters.add(letter)
 
         if letter in self.word:
-            print(f"\n{Fore.green}CORRECT!{Style.reset}")
+            print(f"\n{Fore.GREEN}CORRECT!{Style.RESET}")
             for i, char in enumerate(self.word):
                 if char == letter:
                     self.current_progress[i] = letter
         elif not letter.isalpha() or len(letter) > 1:
-            print(f"\n{Fore.red}Invalid guess! Try again!{Style.reset}")
+            print(f"\n{Fore.RED}Invalid guess! Try again!{Style.RESET}")
         else:
-            print(f"{Fore.red}INCORRECT!{Style.reset}")
+            print(f"{Fore.RED}INCORRECT!{Style.RESET}")
             self.guesses_left -= 1
             self.hangman_stage += 1
 
-
+# Tracking letters guessed and displaying them on screen
     def display_progress(self):
         print(' '.join(self.current_progress))
         print("\nLetters guessed:", ', '.join(sorted(self.guessed_letters))) 
         print(HANGMAN[self.hangman_stage])
 
+# GAME OVER - Function to indicate whether player has won or lost game
     def is_game_over(self):
         if '_' not in self.current_progress:
             clear_terminal()
             print("*************************************")
-            print(f"{Fore.white}{Back.green}\n!!!  YOU WON  !!!\n\nCongratulations!\n{Style.reset}")
-            print("*************************************")
+            print(f"{Fore.RED}{Back.GREEN}\n!!!  YOU WON  !!!\n\nCongratulations!\n{Style.RESET}")
+            print("*************************************\n")
             reset()
             return True
         elif self.guesses_left == 0:
             clear_terminal()
-            print(f"{Fore.white}{Back.red}!!!  GAME OVER  !!!{Style.reset}\n")
+            print(f"{Fore.GREEN}{Back.RED}!!!  GAME OVER  !!!{Style.RESET}\n")
             print(HANGMAN[7])
-            print(f"{Fore.white}{Back.red}\nBetter luck next time, bucko.\nThe correct word was: {self.word}{Style.reset}\n")
+            print(f"{Fore.GREEN}{Back.RED}\nBetter luck next time, bucko.\nThe correct word was: {self.word}{Style.RESET}\n")
             reset()
             return True
         return False
@@ -147,6 +153,42 @@ def get_random_word(category):
         word_list = [line.strip() for line in file]
     return random.choice(word_list)
 
+
+
+def main_menu():
+    print(f"\n{Back.BLUE}LET'S PLAY HANGMAN!{Style.RESET}\n")
+    print("Enter 1 to play game")
+    print("Enter 2 for game instructions")
+    print("Enter 3 to exit game")
+    
+    menu_choice = input("\nSELECT AN OPTION: ")
+
+    valid_choices = ["1", "2", "3"]
+
+    if menu_choice not in valid_choices:
+        print(f"\n{Fore.RED}Invalid option, please try again.{Style.RESET}")
+    elif menu_choice == "1":
+        clear_terminal()
+        game_start()
+    elif menu_choice == "2":
+        clear_terminal()
+        game_help()
+    elif menu_choice == "3":
+        clear_terminal()
+        print("\nSEEYA NEXT TIME!\n")
+        exit()
+
+
+def game_help():
+    print("HOW TO PLAY HANGMAN\n\n")
+    print("Step 1: Select a category from which you would like your hidden word to be selected")
+    print("Step 2: The hidden word you are presented will be represented by underscores. Each underscore will represent a letter. The player ")
+
+        
+
+
+
+# Main game function
 def game_start():
     categories = {
         1: "SPOOKY",
@@ -156,55 +198,60 @@ def game_start():
         5: "CULINARY_DELIGHTS"
     }
 
-    print(f"\n{Back.blue}LET'S PLAY HANGMAN!{Style.reset}")
     print("\nCATEGORIES:\n")
     for num, category in categories.items():
         print(f"{num}. {category}")
+    
+    while True:
+        try:
+            category_choice = int(input("\nEnter a number to select a category: "))
+            selected_category = categories.get(category_choice)
 
-    category_choice = int(input("\nEnter a number to select a category: "))
-    selected_category = categories.get(category_choice)
+            if selected_category:
+                clear_terminal()
+                print(f"You've selected the {Fore.MAGENTA}{selected_category}{Style.RESET} category!\n")
+                word = get_random_word(selected_category)
+                game = Hangman(word)
 
-    if selected_category:
-        clear_terminal()
-        print(f"You've selected the {selected_category} category!\n")
-        word = get_random_word(selected_category)
-        game = Hangman(word)
+                while not game.is_game_over():
+                    print(f"\nYou have {game.guesses_left} guesses remaining\n")
+                    game.display_progress()
+                    guess = input("Guess a letter: ")
 
-        while not game.is_game_over():
-            print(f"\nYou have {game.guesses_left} guesses remaining\n")
-            game.display_progress()
-            guess = input("Guess a letter: ")
-
-            if guess.strip() == "":
-                print(f"\n{Fore.red}Invalid guess! Try again!{Style.reset}")
+                    if guess.strip() == "":
+                        print(f"\n{Fore.RED}Invalid guess! Try again!{Style.RESET}")
+                    else:
+                        game.guess(guess)
+                break
             else:
-                game.guess(guess)
-    else:
-        print(f"{Fore.red}Invalid category choice. Please select a valid category.{Style.reset}")
+                print(f"\n{Fore.RED}Invalid category choice. Please select a valid category.{Style.RESET}")
+        except ValueError:
+            print(f"\n{Fore.RED}Invalid input! Please enter a number.{Style.RESET}")
 
 
 # Asks the player if they want to play again & resets game
 def reset():
     while True:
-        play_again = input("Play again?\n\nY/N")
+        reset = input("Play again?\n\nY/N: ")
+        play_again = reset.upper()
         if play_again == "Y":
+            clear_terminal()
             game_start()
         elif play_again == "N":
             clear_terminal()
-            print("Thanks for playing!\n\nSEEYA NEXT TIME")
+            print("Thanks for playing!\n\nSEEYA NEXT TIME\n")
             break
         else:
-            print(f"\n{Fore.red}Invalid option, please try again.{Style.reset}")
+            print(f"\n{Fore.RED}Invalid option, please try again.{Style.RESET}")
+
+        exit()
 
 if __name__ == "__main__":
-    game_start()
+    main_menu()
 
 # Current issues:
-# Need to write reset function that allows user to play again
-# Error handling to create error message when user selects invalid catgory
-# Error handling - Enter button being taken as a correct guess
 # {Fore.white} not working???
 # Add option to exit program
-# Redirect to categories when invalid category chosen
+
 
 
