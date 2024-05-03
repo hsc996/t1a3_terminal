31781568b8f0c1afb078de098c2b9da38206e382
hsc996 <han.scaife@gmail.com>
2024-05-03 11:33:14 +1000

    setup bash script created

--- setupcheck.sh
+++ setupcheck.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# To check if python3 is installed
 check_python() {
     python_version=$(python3 --version 2>&1)
     if [[ $python_version == *"3"* ]]; then
@@ -9,13 +10,14 @@ check_python() {
     fi
 }
 
+# To check if venv exists/create venv if not
 start_venv() {
-    echo "Creating virtual environment.."
+    echo "Creating new virtual environment..."
     if python3 -m venv .venv; then
         echo "Virtual environment created successfully."
         source .venv/bin/activate
 
-        echo "Installing required dependencies.."
+        echo "Installing required dependencies..."
         if pip3 install -r ./requirements.txt; then
             echo "Dependencies installed successfully."
             return 0 
